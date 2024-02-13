c28ba34b9aedf9402f46f3191f74f9fa7596e909
milanMilicic <milanm.milicic@gmail.com>
2024-02-13 18:34:58 +0100

    Change the title depending on what the url is

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -2,8 +2,37 @@ import { Link } from "react-router-dom"
 import { FaChevronLeft } from 'react-icons/fa6'
 import './Categories.css'
 import { Outlet } from "react-router";
+import { useLocation } from 'react-router-dom';
 
 function Categories() {
+
+    let title = 'ALL';
+
+    let location = useLocation();
+
+    switch (location.pathname) {
+        case '/categories/furniture':
+            title = 'FURNITURE';
+            break;
+        case '/categories/electronics':
+            title = 'ELECTRONICS';
+            break;
+        case '/categories/lamps':
+            title = 'LAMPS';
+            break;
+        case '/categories/kitchen':
+            title = 'KITCHEN';
+            break;
+        case '/categories/chairs':
+            title = 'CHAIRS';
+            break;
+        case '/categories/skin-care':
+            title = 'SKIN CARE';
+            break;
+        default:
+            title = 'ALL'
+    }
+
     return (
         <>
             <section className="categories">
@@ -13,7 +42,7 @@ function Categories() {
                             <Link to="/">
                                 <FaChevronLeft /> Home
                             </Link>
-                            <h3>ALL</h3>
+                            <h3>{title}</h3>
                         </div>
                         <div className="categories-links">
                             <Link to='all'>All</Link>
