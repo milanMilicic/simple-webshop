22902a6eb2019718b4b8efe5dabf237cd70994d8
milanMilicic <milanm.milicic@gmail.com>
2024-02-13 14:30:14 +0100

    Categories filter

---
+++ src/pages/Categories/Categories.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom"
+import { FaChevronLeft } from 'react-icons/fa6'
+import './Categories.css'
+import { Outlet } from "react-router";
+
+function Categories() {
+    return (
+        <>
+            <section className="categories">
+                <div className="container">
+                    <div className="categories-content">
+                        <div className="categories-title">
+                            <Link to="/">
+                                <FaChevronLeft /> Home
+                            </Link>
+                            <h3>ALL</h3>
+                        </div>
+                        <div className="categories-links">
+                            <Link to='all'>All</Link>
+                            <Link to='furnitures'>Furnitures</Link>
+                            <Link to='electronics'>Electronics</Link>
+                            <Link to='lamps'>Lamps</Link>
+                            <Link to='kitchen'>Kitchen</Link>
+                            <Link to='chairs'>Chairs</Link>
+                            <Link to='skin-care'>Skin Care</Link>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Outlet />
+        </>
+    )
+}
+export default Categories
